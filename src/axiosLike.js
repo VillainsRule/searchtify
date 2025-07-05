d80e4310d59972ae8560823b5e3a0024392adf1d
thonk <hello@villainsrule.xyz>
2025-07-05 13:51:16 -0400

    1.2.6: improve axiosLike

--- src/axiosLike.js
+++ src/axiosLike.js
@@ -1,29 +1,11 @@
-const get = (url, params) => new Promise((r) => {
+const handle = (method, url, body, params) => new Promise((r) => {
     const response = {};
 
-    fetch(url, params).then((res) => {
-        response.headers = res.headers;
-        response.status = res.status;
-        response.statusText = res.statusText;
-
-        res.text().then((text) => {
-            try {
-                response.data = JSON.parse(text);
-            } catch {
-                response.data = text;
-            }
-
-            r(response);
-        });
-    });
-});
-
-const post = (url, body, params) => new Promise((r) => {
-    const response = {};
+    const body2 = body ? { body: typeof body === 'object' ? JSON.stringify(body) : body } : {};
 
     fetch(url, {
-        method: 'POST',
-        body: typeof body === 'object' ? JSON.stringify(body) : body,
+        method,
+        ...body2,
         ...params
     }).then((res) => {
         response.headers = res.headers;
@@ -36,10 +18,12 @@ const post = (url, body, params) => new Promise((r) => {
             } catch {
                 response.data = text;
             }
-
             r(response);
         });
     });
 });
+
+const get = (url, params) => handle('GET', url, null, params);
+const post = (url, body, params) => handle('POST', url, body, params);
 
 export const axiosLike = { get, post };
